9e8464583891dbe42ddf78b9bf15337eae683a71
Maksim Fedoseev <114389643+fedoseevm@users.noreply.github.com>
2024-10-07 07:54:52 +0200

    Update Program.cs

--- Dziedziczenie/System_Zarzadzania_Biblioteka/Program.cs
+++ Dziedziczenie/System_Zarzadzania_Biblioteka/Program.cs
@@ -47,11 +47,57 @@ public class Reader : Person
 	{
 		BorrowedBooksList = new List<Book>();
 	}
-	public void AddBook(Book book)
+	public void BorrowBook(Book book)
 	{
 		BorrowedBooksList.Add(book);
         Console.WriteLine($"Czytelnik {FirstName} {LastName} wyporzyczył książkę: {book.Title}");
     }
+}
+
+public class Library
+{
+	public List<Book> BooksList { get; set; }
+	public List<Author> AuthorsList { get; set; }
+	public List<Reader> ReadersList { get; set; }
+	public Library()
+	{
+		BooksList = new List<Book>();
+		ReadersList = new List<Reader>();
+		AuthorsList = new List<Author>();
+	}
+
+	public void AddBook(Book book)
+	{
+		BooksList.Add(book);
+	}
+	public void AddReader(Reader reader)
+	{
+		ReadersList.Add(reader);
+	}
+	public void AddAuthor(Author author)
+	{
+		AuthorsList.Add(author);
+	}
+	public void BorrowBook(Reader reader, Book book)
+	{
+		if (BooksList.Contains(book))
+		{
+			reader.BorrowBook(book);
+			BooksList.Remove(book);
+		}
+		else
+		{
+			Console.WriteLine("Książka nie jest dostępna w bibliotece.");
+		}
+	}
+	public void DisplayBooks()
+	{
+		Console.WriteLine("Książki dostępne w bibliotece:");
+		foreach (var book in BooksList)
+		{
+			Console.WriteLine($"{book.Title} - {book.Author.FirstName} ")
+		}
+	}
 }
 
 namespace System_Zarzadzania_Biblioteka
